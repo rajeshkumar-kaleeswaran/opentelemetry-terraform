66aed071773f64f0a40c3c2c01d1fe0cf69211f5
anwercloud <anwershaik.cloud@gmail.com>
2025-09-12 12:57:49 +0530

    added script for aws_intance:ec2

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -2,11 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "project_instance" {
+  ami           = "ami-0360c520857e3138f" # ubuntu AMI (HVM), SSD Volume Type
+  instance_type = "t2.medium"
+  associate_public_ip_address = true
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "MyProjectInstance"
+  }
+}
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "my-project-terraform-eks-state-s3-bucket"
 
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 }
 
--- eks-install/backend/outputs.tf
+++ eks-install/backend/outputs.tf
@@ -7,3 +7,15 @@ output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_locks.id
   description = "The name of the DynamoDB table"
 }
+output "instance_id" {
+  value       = aws_instance.project_instance.id
+  description = "The ID of the EC2 instance"
+}
+output "nametag" {
+  value       = aws_instance.project_instance.tags["Name"]
+  description = "The Name tag of the EC2 instance"
+}
+output "public_ip" {
+  value       = aws_instance.project_instance.public_ip
+  description = "The public IP address of the EC2 instance"
+}

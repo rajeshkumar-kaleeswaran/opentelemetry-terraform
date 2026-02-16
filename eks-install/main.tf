419e3d64b588b7c2c2ebeb8aafe9cfe9d78c46cc
Ubuntu <ubuntu@ip-172-31-46-119.ap-south-1.compute.internal>
2026-02-16 10:21:45 +0000

    Chnages made by me

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket"
+    bucket         = "my-tf-test-telemetry-bucket"
     key            = "terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-eks-state-locks"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-telemetry-table"
     encrypt        = true
   }
 }
